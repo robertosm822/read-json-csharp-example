95440a9f3582e0a2352434b3176fe16832de9e3b
robertosm822 <robertomelo822@gmail.com>
2020-03-06 19:17:25 -0300

    Create read JSON URL

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
+using System.Net;
 using System.Web;
 
 namespace MyFirstConsole
@@ -12,6 +14,41 @@ namespace MyFirstConsole
         public string produto { get; set; }
         public string situacao { get; set; }
     }
+    class Users
+    {
+        /*
+            Campos escolhidos da API:
+            "id": 1,
+            "name": "Leanne Graham",
+            "username": "Bret",
+         */
+         public string id { get; set; }
+         public string name { get; set; }
+         public string username { get; set; }
+    }
+
+    public class ListUsersFormJson
+    {
+        
+        public void load() {
+            // consumindo api json
+            string jsonUrl = new WebClient().DownloadString("https://jsonplaceholder.typicode.com/users");
+            Users[] users = JsonConvert.DeserializeObject<Users[]>(jsonUrl);
+
+            // mostrando os usuarios
+            Console.WriteLine("__________________________");
+            Console.WriteLine("==== Usuarios - List ====");
+            Console.WriteLine("__________________________");
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.id);
+                Console.WriteLine(user.name);
+                Console.WriteLine(user.username);
+                Console.WriteLine("__________________________");
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -19,9 +56,6 @@ namespace MyFirstConsole
             // Por padrao o Programa vai ler o arquivo que estah na pasta /bin/Debug/netcoreapp3.1/
             // para corrigir, basta sair das 3 pastas como abaixo:
             var json = File.ReadAllText("../../../dados.json");
-            
- 
-            // JavaScriptSerializer serialized = new JavaScriptSerializer();
             var serialized = JObject.Parse(json);
             var tanquesObjects = serialized["tanques"].ToObject<Tanques[]>();
 
@@ -33,6 +67,8 @@ namespace MyFirstConsole
                 Console.WriteLine("__________________________");
             }
 
+            // carregar lista de Users de URL json
+            new ListUsersFormJson().load();
 
             Console.ReadLine();
         }
