d42712df8ddcc3a288cdc9b9ab8a61111315d2d5
robertosm822 <robertomelo822@gmail.com>
2020-03-06 17:30:49 -0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Web;
+
+namespace MyFirstConsole
+{
+
+    class Tanques
+    {
+        public string tipo { get; set; }
+        public string produto { get; set; }
+        public string situacao { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("This is my First Console App \n Digite seu nome");
+           // string resposta = Console.ReadLine();
+           // Console.WriteLine("Hello,"+ resposta);
+            var json = File.ReadAllText("dados.json");
+            
+ 
+            // JavaScriptSerializer serialized = new JavaScriptSerializer();
+            var serialized = JObject.Parse(json);
+            var tanquesObjects = serialized["tanques"].ToObject<Tanques[]>();
+
+            foreach (var tanque in tanquesObjects)
+            {
+                Console.WriteLine(tanque.produto);
+                Console.WriteLine(tanque.situacao);
+                Console.WriteLine(tanque.tipo);
+                Console.WriteLine("__________________________");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
